19c6d0d1905cdd3741f7a638c52d2b867b06daac
gulcedogruoz <168816785+gulcedogruoz@users.noreply.github.com>
2025-05-19 23:11:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOcelot();
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+var app = builder.Build();
+
+await app.UseOcelot(); 
+
+app.Run();
+
+
